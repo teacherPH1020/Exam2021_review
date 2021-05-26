95f9e933a631c273a9eeccf22a2644c7e66af602
PH1020 teacher Sergey P <postnikov_s@teacher.itsrep.by>
2021-05-26 21:52:38 +0300

    add added

--- WeZdoms/urls.py
+++ WeZdoms/urls.py
@@ -1,13 +1,13 @@
 """WeZdoms URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to view. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function view
+    1. Add an import:  from my_app import view
+    2. Add a URL to urlpatterns:  path('', view.home, name='home')
+Class-based view
+    1. Add an import:  from other_app.view import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from rel import views
 
 urlpatterns = [
+    path('rel/', views.index, name='rel'),
     path('', TemplateView.as_view(template_name="home.html")),
     path('admin/', admin.site.urls),
 ]
--- rel/views.py
+++ rel/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .view import add
+from django.http import HttpResponse
+
+
+# Create your view here.
+def index(request):
+    a=3
+    b=4
+    c = add.add_2(a,b)
+    return HttpResponse(str(c))
